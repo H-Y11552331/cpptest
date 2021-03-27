65abaad2e9860b5346c48f8174b6b83be2f75500
Hyuga <hyu.1155.2331@icloud.com>
2021-03-27 14:45:49 +0900

    master

---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "enter your name: ";
+    string name;
+    cin >> name;
+    cout << "楽しい" << 5 << "years\n" << name << "\n";
+    return 0;
+}
